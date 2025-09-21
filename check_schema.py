e0d5675ad48481d3f9fea98090af6ff9bdaa6ecc
Gord Tulloch <gord.tulloch@gmail.com>
2025-09-21 12:59:28 -0500

    Update

--- check_schema.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('astrofiler.db')
-cursor = conn.execute('PRAGMA table_info(fitssession)')
-print('FitsSession table structure:')
-for row in cursor.fetchall():
-    print(f'  {row[1]} ({row[2]})')
-conn.close()
